c49b1b5280920254f462394129c66e8c4be18787
Kaspars <kaspars@metronet.no>
2012-08-24 17:41:10 +0200

    add external script async loading
    Signed-off-by: Kaspars <kaspars@metronet.no>

--- minit.php
+++ minit.php
@@ -3,7 +3,7 @@
 Plugin Name: Minit
 Plugin URI: http://konstruktors.com
 Description: Combine JS and CSS files and serve them from the upload's folder
-Version: 0.4
+Version: 0.5
 Author: Kaspars Dambis
 Author URI: http://konstruktors.com
 */
@@ -131,7 +131,6 @@ function init_minit_css() {
 	wp_enqueue_style( 'minit-css', apply_filters( 'minit_url_js', $combined_file_url ), null, null );
 }
 
-
 add_action( 'admin_init', 'purge_minit_cache' );
 
 function purge_minit_cache() {
@@ -143,5 +142,50 @@ function purge_minit_cache() {
 	foreach ( glob( $wp_upload_dir['basedir'] . '/minit/*.*' ) as $minit_file )
 		unlink( $minit_file );
 
+}
+
+
+/**
+ * Print external scripts asynchronously in the footer, using a method similar to Google Analytics
+ */
+
+add_action( 'wp_print_footer_scripts', 'add_footer_scripts_async', 5 );
+
+function add_footer_scripts_async() {
+	global $wp_scripts;
+
+	$wp_scripts->async = array();
+
+	foreach ( $wp_scripts->queue as $handle ) {
+		if ( in_array( $handle, $wp_scripts->in_footer ) && preg_match( '|^(http(s)?\:)?\/\/|i', str_replace( home_url(), '', $wp_scripts->registered[$handle]->src ) ) ) {
+			$wp_scripts->async[] = $handle;
+			wp_dequeue_script( $handle );
+		}
+	}
+}
+
+add_action( 'wp_print_footer_scripts', 'print_footer_scripts_async', 20 );
+
+function print_footer_scripts_async() {
+	global $wp_scripts;
+
+	if ( empty( $wp_scripts->async ) )
+		return;
+
+	?>
+	<script id="async-scripts" type="text/javascript">
+	(function() {
+		var js, fjs = document.getElementById('async-scripts'),
+			add = function( url, id ) {
+				js = document.createElement('script'); js.type = 'text/javascript'; js.src = url; js.async = true; js.id = id;
+				fjs.parentNode.insertBefore(js, fjs);
+			};
+		<?php 
+		foreach ( $wp_scripts->async as $handle )
+			printf( 'add("%s", "%s"); ', $wp_scripts->registered[$handle]->src, 'async-script-' . $handle ); 
+		?>
+	})();
+	</script>
+	<?php
 }
 
