31104ba5de75af57030e46e575d469ee1cb32724
Kaspars <kaspars@konstruktors.com>
2014-02-13 13:34:15 +0200

    Version bump

--- minit.php
+++ minit.php
@@ -4,7 +4,7 @@ Plugin Name: Minit
 Plugin URI: https://github.com/kasparsd/minit
 GitHub URI: https://github.com/kasparsd/minit
 Description: Combine JS and CSS files and serve them from the uploads folder
-Version: 0.8.6
+Version: 0.8.7
 Author: Kaspars Dambis
 Author URI: http://kaspars.net
 */
