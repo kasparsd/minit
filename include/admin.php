55e8188be5a14112ee099eabd60b9ebdf1405df4
Kaspars <hi@kaspars.net>
2015-12-14 20:58:57 +0200

    Intend using tabs instead

--- include/admin.php
+++ include/admin.php
@@ -2,64 +2,64 @@
 
 class Minit_Admin {
 
-  protected $plugin;
+	protected $plugin;
 
 
-  public function __construct( $plugin ) {
+	public function __construct( $plugin ) {
 
-    $this->plugin = $plugin;
+		$this->plugin = $plugin;
 
-  }
-
-
-  public function init() {
-
-    // Add a Purge Cache link to the plugin list
-    // @todo Enable this for multisite somehow
-    add_filter( 'plugin_action_links_' . plugin_basename( $this->plugin->plugin_file ), array( $this, 'plugin_action_link_cache_bump' ) );
-
-    // Maybe purge minit cache
-    add_action( 'admin_init', array( $this, 'cache_bump' ) );
-
-  }
+	}
 
 
-  function plugin_action_link_cache_bump( $links ) {
+	public function init() {
 
-  	$links[] = sprintf(
-  			'<a href="%s">%s</a>',
-  			wp_nonce_url( add_query_arg( 'purge_minit', true ), 'purge_minit' ),
-  			__( 'Purge cache', 'minit' )
-  		);
+		// Add a Purge Cache link to the plugin list
+		// @todo Enable this for multisite somehow
+		add_filter( 'plugin_action_links_' . plugin_basename( $this->plugin->plugin_file ), array( $this, 'plugin_action_link_cache_bump' ) );
 
-  	return $links;
+		// Maybe purge minit cache
+		add_action( 'admin_init', array( $this, 'cache_bump' ) );
 
-  }
-
-
-  function cache_bump() {
-
-    if ( ! isset( $_GET['purge_minit'] ) )
-      return;
-
-    if ( ! check_admin_referer( 'purge_minit' ) )
-		  return;
-
-    $this->plugin->cache_bump();
-
-    add_action( 'admin_notices', array( $this, 'cache_bump_notice' ) );
-
-  }
+	}
 
 
-  function cache_bump_notice() {
+	function plugin_action_link_cache_bump( $links ) {
 
-  	printf(
-  		'<div class="updated"><p>%s</p></div>',
-  		__( 'Success: Minit cache purged.', 'minit' )
-  	);
+		$links[] = sprintf(
+				'<a href="%s">%s</a>',
+				wp_nonce_url( add_query_arg( 'purge_minit', true ), 'purge_minit' ),
+				__( 'Purge cache', 'minit' )
+			);
 
-  }
+		return $links;
+
+	}
+
+
+	function cache_bump() {
+
+		if ( ! isset( $_GET['purge_minit'] ) )
+			return;
+
+		if ( ! check_admin_referer( 'purge_minit' ) )
+			return;
+
+		$this->plugin->cache_bump();
+
+		add_action( 'admin_notices', array( $this, 'cache_bump_notice' ) );
+
+	}
+
+
+	function cache_bump_notice() {
+
+		printf(
+			'<div class="updated"><p>%s</p></div>',
+			__( 'Success: Minit cache purged.', 'minit' )
+		);
+
+	}
 
 
 }
