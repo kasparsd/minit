eee07529edfb0aa20e42f11f05844805b95131c1
Kaspars <hi@kaspars.net>
2015-12-14 20:45:53 +0200

    Adjust line endings

--- minit.php
+++ minit.php
@@ -4,7 +4,7 @@ Plugin Name: Minit
 Plugin URI: https://github.com/kasparsd/minit
 GitHub URI: https://github.com/kasparsd/minit
 Description: Combine JS and CSS files and serve them from the uploads folder.
-Version: 1.3-beta
+Version: 1.3-beta.2
 Author: Kaspars Dambis
 Author URI: http://kaspars.net
 */
@@ -13,7 +13,7 @@ add_action( 'plugins_loaded', array( 'Minit_Plugin', 'instance' ) );
 
 class Minit_Plugin {
 
-  public $version = 'minit-1.3';
+  public $revision = '20151214';
   public $plugin_file;
 
 
