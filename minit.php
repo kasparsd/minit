26da03a2460b2c080668ac7d38ac3f0b596ac777
Kaspars Dambis <hi@kaspars.net>
2016-08-28 15:30:36 +0300

    Prep for 1.3 release

--- minit.php
+++ minit.php
@@ -4,7 +4,7 @@ Plugin Name: Minit
 Plugin URI: https://github.com/kasparsd/minit
 GitHub URI: https://github.com/kasparsd/minit
 Description: Combine JS and CSS files and serve them from the uploads folder.
-Version: 1.3-beta.3
+Version: 1.3
 Author: Kaspars Dambis
 Author URI: http://kaspars.net
 */
