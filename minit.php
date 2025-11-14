a72b0781cacf32b4955fbb2586960872c4d947a2
Kaspars Dambis <hi@kaspars.net>
2025-11-14 14:22:59 +0200

    Just in case

--- minit.php
+++ minit.php
@@ -3,6 +3,7 @@
  * Plugin Name: Minit
  * Plugin URI: https://github.com/kasparsd/minit
  * GitHub URI: https://github.com/kasparsd/minit
+ * GitHub Plugin URI: https://github.com/kasparsd/minit
  * Update URI: https://updates.wpelevator.com/wp-json/update-pilot/v1/plugins
  * Description: Combine JS and CSS files and serve them from the uploads folder.
  * Version: 2.1.0
