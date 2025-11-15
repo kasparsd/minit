bf441a8de8262170412069003cc3d13caffa9a59
Kaspars Dambis <hi@kaspars.net>
2025-11-15 19:08:59 +0200

    Test import resolver

--- tests/phpunit/class-minit-css-test.php
+++ tests/phpunit/class-minit-css-test.php
@@ -29,28 +29,32 @@ class Minit_CSS_Test extends WP_UnitTestCase {
 		);
 	}
 
+	private function url_to( string $pattern ): string {
+		return sprintf( $pattern, home_url() );
+	}
+
 	public function test_resolve_urls() {
 		$minit_css = new Minit_Css(
 			Minit_Plugin::instance(),
 			new Minit_Asset_Cache( WP_CONTENT_DIR . '/minit-test', 'version' )
 		);
 
-		wp_enqueue_style( 'minit-css-media-default', 'https://example.com/default.css' );
+		wp_enqueue_style( 'minit-css-url-paths', 'https://example.com/default.css' );
 
 		$this->assertEquals(
-			'body { background-image: url(\'http://localhost:8888/path/to/path/to/image.png\'); }',
+			$this->url_to( 'body { background-image: url(\'%s/path/to/path/to/image.png\'); }' ),
 			$minit_css->minit_item( 'body { background-image: url( "path/to/image.png" ); }', 'minit-css', '/path/to/css.css' ),
 			'relative paths wrapped in quotes'
 		);
 
 		$this->assertEquals(
-			'body { background-image: url(\'http://localhost:8888/path/to/direct/image.png\'); }',
+			$this->url_to( 'body { background-image: url(\'%s/path/to/direct/image.png\'); }' ),
 			$minit_css->minit_item( 'body { background-image: url(direct/image.png); }', 'minit-css', '/path/to/css.css' ),
 			'relative paths without quotes'
 		);
 
 		$this->assertEquals(
-			'body { background-image: url(\'http://localhost:8888/path/to/some/image.jpeg\'); }',
+			$this->url_to( 'body { background-image: url(\'%s/path/to/some/image.jpeg\'); }' ),
 			$minit_css->minit_item( 'body { background-image: url(  "some/image.jpeg\'    ); }', 'minit-css', '/path/to/css.css' ),
 			'mixed quotes and spaces'
 		);
@@ -65,6 +69,27 @@ class Minit_CSS_Test extends WP_UnitTestCase {
 			'body { background-image: url( "http://example.jpeg" ); }',
 			$minit_css->minit_item( 'body { background-image: url( "http://example.jpeg" ); }', 'minit-css', '/path/to/css.css' ),
 			'absolute urls are kept intact'
+		);
+	}
+
+	public function test_resolve_imports() {
+		$minit_css = new Minit_Css(
+			Minit_Plugin::instance(),
+			new Minit_Asset_Cache( WP_CONTENT_DIR . '/minit-test', 'version' )
+		);
+
+		wp_enqueue_style( 'minit-css-imports', 'https://example.com/imports.css' );
+
+		$this->assertEquals(
+			$this->url_to( '@import url(\'%s/path/to/my-imported-styles.css\');' ),
+			$minit_css->minit_item( '@import "my-imported-styles.css";', 'minit-css', '/path/to/my-imported-styles.css' ),
+			'relative imports are made absolute'
+		);
+
+		$this->assertEquals(
+			$this->url_to( '@import url(\'%s/path/relative/relative/path.css\');' ),
+			$minit_css->minit_item( '@import url("relative/path.css");', 'minit-css', '/path/relative/path.css' ),
+			'relative url() imports are made absolute'
 		);
 	}
 }
